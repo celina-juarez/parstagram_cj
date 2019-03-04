1e9d5103a334ceb35b02684595499abf0cd42f63
Celina Juarez <cejuarez@csumb.edu>
2019-03-04 15:00:03 -0800

    Displaying comments

---
+++ parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  parstagram
+//
+//  Created by Celina Juarez on 3/4/19.
+//  Copyright © 2019 Celina Juarez. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
